5d4a7c14d6f8f7d3bd1917138360e2565c9fa40e
Worawit Penglam <worawit.pen@student.mahidol.edu>
2022-07-04 23:10:58 +0700

    create a fucntion that check that index can place the number or not

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,35 @@ use std::fs::File;
 use std::io;
 use std::io::*;
 
+fn is_save(grid: Vec<Vec<u64>>, num: u64, row: usize, col: usize) -> bool{
+    let size = grid[0].len();
+    for i in size{
+        if grid[row][i] == num{
+            return false
+        }
+    }
+
+    for j in size{
+        if grid[j][col] == num{
+            return false
+        }
+    }
+
+    let rowChop = row - row%3;
+    let colChop = col - col%3;
+    let mut i = 0;
+    let mut j = 0;
+    while i < 3{
+        while j < 3{
+            if grid[i+rowChop][j+colChop] == num{
+                return false
+            }
+        }
+    }
+
+    return true;
+}
+
 fn main() -> io::Result<()> {
     let file = File::open("Test//map1.txt").unwrap();
     let reader = BufReader::new(file);
