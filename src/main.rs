daa709e33bcbc0ffc8a87fa53eeab4dbb2631b38
Worawit Penglam <worawit.pen@student.mahidol.edu>
2022-07-07 14:52:13 +0700

    Use some cool api to solve it(basically it done)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::fs::File;
 use std::io;
 use std::io::*;
-use rand::Rng;
+use array2d::Array2D;
+use board::*;
+use sudoku_solver::*;
 
 
-fn is_safe(mut grid: &Vec<Vec<u32>>, num: u32, row: usize, col: usize) -> bool{
+fn is_save(mut grid: &Vec<Vec<u32>>, num: u32, row: usize, col: usize) -> bool{
     let size = grid[0].len();
     let mut error = 0;
     if grid[row].contains(&num){
@@ -49,14 +51,23 @@ fn solveGrid(grid: &mut Vec<Vec<u32>>, mut row: usize, mut col: usize) -> bool{
 
         return solveGrid(grid, row, col + 1);
     }
+    let mut time = 0;
+    // while time < 20{
+    //
+    // //loop{
+    //     let mut rng = rand::thread_rng();
+    //     let num = rng.gen_range(1, 10);
 
     for num in 1..=9 {
-        if is_safe(&grid, num, row, col) {
+        if is_save(&grid, num, row, col) {
             grid[row][col] = num;
+            println!("{} put at {} {}", num, row, col);
+            println!("");
             if solveGrid(grid, row,col + 1) {
                 return true
             }
         }
+
         grid[row][col] = 0;
     }
     return false;
@@ -64,42 +75,68 @@ fn solveGrid(grid: &mut Vec<Vec<u32>>, mut row: usize, mut col: usize) -> bool{
 
 
 fn main() -> io::Result<()> {
-    let file = File::open("Test//map1.txt").unwrap();
+    let file = File::open("Test//map2.txt").unwrap();
     let reader = BufReader::new(file);
-    let mut grid = vec![];
+    let mut grid = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+    let mut indexs = 0;
     for line in reader.lines(){
-        let mut row = Vec::new();
+        let mut row = [0,0,0,0,0,0,0,0,0];
+        let mut index = 0;
         for c in line.unwrap().chars(){
+            if index >9{
+                index = 0;
+            }
             if c != ',' {
-                let x = c.to_digit(10).unwrap();
-                row.push(x);
+                let x = c.to_digit(10).unwrap() as u8;
+                row[index] = x;
+                index += 1;
             }
         }
 
-        grid.push(row);
+        grid[indexs] = row;
+        indexs += 1;
     }
-    let mut newGrid = grid.clone();// Copy the old grid to solve it and another will be the unsolve. And use it to compare
-    // grid sudoku <= unsolve
-    //newGrid sudoku <= solved
 
 
-    if solveGrid(&mut newGrid,0,0){
-        for line in newGrid{
-            for c in line{
-                print!("{}, ", c);
-            }
-            println!("");
-        }
 
+
+
+
+
+    let board = Board::from(&grid);
+
+    let mut solutions = SolutionIter::new(&board);
+
+    // for x in solutions.into_iter() {
+    //
+    //     println!("{}",x);
+    //     println!("");
+    // }
+
+    println!("{}",solutions.count());
+
+    if let Some(solution) = solve(&board) {
+        println!("Solution:\n{}\n", solution);
+    } else {
+        println!("No solution found.");
     }
-    println!("");
 
-    for line in grid{
-        for c in line{
-            print!("{}, ", c);
-        }
-        println!("");
-    }
+    // let mut newGrid = grid.clone();
+    //
+    //
+    //
+    // if solveGrid(&mut newGrid,0,0){
+    //     for lel in newGrid{
+    //         for olo in lel{
+    //             print!("{}, ", olo);
+    //         }
+    //         println!("");
+    //     }
+    //
+    // }
+    // println!("");
+
+
 
 
 
